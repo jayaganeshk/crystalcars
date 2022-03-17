a531841f7dfd2976abfebe8ffcf01be65f21c479
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-03-18 00:19:04 +0530

    remove useless `console.log`

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 
-console.log(process.env.PUBLIC_PATH);
-
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   publicPath: process.env.PUBLIC_PATH ?? "/",
